dabed6a44248382a8f884a598afcfe9cfdded557
gracemoore1 <B00153274@mytudublin.ie>
2026-02-26 01:39:21 +0000

    Student header: left-align titles & remove duplicate header hamburger

--- resources/views/student/support.blade.php
+++ resources/views/student/support.blade.php
@@ -1,15 +1,8 @@
 <x-app-layout>
 
     <x-slot name="header">
-        <div class="flex items-start justify-between">
-            <button type="button" class="text-slate-700 hover:text-slate-900" aria-label="Menu">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6"
-                     fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M4 6h16M4 12h16M4 18h16" />
-                </svg>
-            </button>
-            <div class="text-right">
+        <div class="flex items-start justify-start">
+            <div class="text-left">
                 <div class="text-2xl font-extrabold text-blue-600 leading-none">RentConnect</div>
                 <div class="mt-1 font-semibold text-gray-800">Support</div>
             </div>
